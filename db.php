9351c67c6d0ac05679a8fc83a1c9fe6528308b44
xinxin1102 <649177898@qq.com>
2017-02-21 09:09:32 +0800

    ef

---
+++ db.php
@@ -0,0 +1,5 @@
+<?php
+sleep(2);
+$db=new mysqli("localhost","root","","w1610");
+$db->query("set names utf8");
+?>
---
+++ insert.php
@@ -0,0 +1,10 @@
+<?php
+include "db.php";
+$id=$_GET["id"];
+$attr=$_GET["attr"];
+$value=$_GET["value"];
+$db->query("update stu set $attr='$value' where id=".$id);
+if($db->affected_rows>0){
+    echo "ok";
+}
+?>
